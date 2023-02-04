6484a4389d492f2be0ec56821c5864a9ccf6d014
Halocaridina <talonpollard@gmail.com>
2023-02-03 17:21:30 -0800

    Rename folders

--- pi/ml/detect.py
+++ pi/ml/detect.py
@@ -59,7 +59,7 @@ def run(model: str, camera_id: int, width: int, height: int, num_threads: int,
   base_options = core.BaseOptions(
       file_name=model, use_coral=enable_edgetpu, num_threads=num_threads)
   detection_options = processor.DetectionOptions(
-      max_results=3, score_threshold=0.6)
+      max_results=2, score_threshold=0.6)
   options = vision.ObjectDetectorOptions(
       base_options=base_options, detection_options=detection_options)
   detector = vision.ObjectDetector.create_from_options(options)
